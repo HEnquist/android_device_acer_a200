b38a28f5ad55930ad1762d636b20fdf93a13d37f
HEnquist <henrik.enquist@gmail.com>
2014-02-05 21:40:26 +0100

    More changes for A200 (picasso_e)

--- a200.mk
+++ a200.mk
@@ -54,8 +54,8 @@ PRODUCT_PACKAGES += \
 
 # ramdisk
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/ramdisk/init.picasso.usb.rc:root/init.picasso_e.usb.rc \
-    $(LOCAL_PATH)/ramdisk/ueventd.picasso.rc:root/ueventd.picasso_e.rc \
+    $(LOCAL_PATH)/ramdisk/init.picasso_e.usb.rc:root/init.picasso_e.usb.rc \
+    $(LOCAL_PATH)/ramdisk/ueventd.picasso_e.rc:root/ueventd.picasso_e.rc \
     $(LOCAL_PATH)/ramdisk/init.picasso.rc:root/init.picasso_e.rc \
     $(LOCAL_PATH)/ramdisk/fstab.picasso:root/fstab.picasso_e 
     
--- cm.mk
+++ cm.mk
@@ -16,8 +16,8 @@ PRODUCT_MANUFACTURER := Acer
 
 #Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=picasso \
-    BUILD_FINGERPRINT="acer/a200_ww_gen1/picasso:4.0.3/IML74K/1329824557:user/release-keys" \
+    PRODUCT_NAME=picasso_e \
+    BUILD_FINGERPRINT="acer/a200_ww_gen1/picasso_e:4.0.3/IML74K/1329824557:user/release-keys" \
     PRIVATE_BUILD_DESC="a200_ww_gen1-user 4.0.3 IML74K 1329824557 release-keys"
 
 # Bootanimation
