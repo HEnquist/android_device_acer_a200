0d67a28ed27760ca06d0971892890a0932b91267
HEnquist <henrik.enquist@gmail.com>
2014-02-08 17:18:14 +0100

    Fixing typos and missing files
    Change-Id: I038a884e9414362a09591ddcd1a03924e97f013e

--- a200.mk
+++ a200.mk
@@ -56,8 +56,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/ramdisk/init.picasso_e.usb.rc:root/init.picasso_e.usb.rc \
     $(LOCAL_PATH)/ramdisk/ueventd.picasso_e.rc:root/ueventd.picasso_e.rc \
-    $(LOCAL_PATH)/ramdisk/init.picasso.rc:root/init.picasso_e.rc \
-    $(LOCAL_PATH)/ramdisk/fstab.picasso:root/fstab.picasso_e 
+    $(LOCAL_PATH)/ramdisk/init.picasso_e.rc:root/init.picasso_e.rc \
+    $(LOCAL_PATH)/ramdisk/fstab.picasso_e:root/fstab.picasso_e 
     
 # firmware
 PRODUCT_COPY_FILES += \
