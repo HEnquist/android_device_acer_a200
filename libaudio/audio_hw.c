605b1427ae709c25218ec86589b0e86003eadd47
Henrik Enquist <henrik.enquist@gmail.com>
2014-02-17 15:30:29 +0100

    cleaning up unused bits for audio

--- libaudio/audio_hw.c
+++ libaudio/audio_hw.c
@@ -178,17 +178,13 @@ static void select_devices(struct audio_device *adev)
 {
     int headphone_on;
     int speaker_on;
-    int docked;
     int main_mic_on;
-    int hdmi_on;
     int headset_mic_on;
 
     headphone_on = adev->out_device & (AUDIO_DEVICE_OUT_WIRED_HEADSET |
                                     AUDIO_DEVICE_OUT_WIRED_HEADPHONE);
     speaker_on = adev->out_device & AUDIO_DEVICE_OUT_SPEAKER;
-    docked = adev->out_device & AUDIO_DEVICE_OUT_ANLG_DOCK_HEADSET;
     main_mic_on = adev->in_device & AUDIO_DEVICE_IN_BUILTIN_MIC;
-    hdmi_on = adev->out_device & AUDIO_DEVICE_OUT_AUX_DIGITAL;
     headset_mic_on = adev->in_device & AUDIO_DEVICE_IN_WIRED_HEADSET;
 
     reset_mixer_state(adev->ar);
@@ -197,26 +193,24 @@ static void select_devices(struct audio_device *adev)
         audio_route_apply_path(adev->ar, "speaker");
     if (headphone_on)
         audio_route_apply_path(adev->ar, "headphone");
-    if (docked)
-        audio_route_apply_path(adev->ar, "dock");
 
     if (headset_mic_on) {
     	audio_route_apply_path(adev->ar, "headset-mic");
     }
     else {
-      if (main_mic_on) {
-        if (adev->orientation == ORIENTATION_LANDSCAPE)
-            audio_route_apply_path(adev->ar, "main-mic-left");
-        else
-            audio_route_apply_path(adev->ar, "main-mic-top");
-      }
+        if (main_mic_on) {
+            if (adev->orientation == ORIENTATION_LANDSCAPE)
+                audio_route_apply_path(adev->ar, "main-mic-left");
+            else
+                audio_route_apply_path(adev->ar, "main-mic-top");
+        }
     }
 
 
     update_mixer_state(adev->ar);
 
-    ALOGV("hp=%c speaker=%c dock=%c hdmi=%c main-mic=%c", headphone_on ? 'y' : 'n',
-          speaker_on ? 'y' : 'n', docked ? 'y' : 'n', hdmi_on ? 'y' : 'n', main_mic_on ? 'y' : 'n');
+    ALOGV("hp=%c speaker=%c main-mic=%c headset-mic=%c", headphone_on ? 'y' : 'n',
+          speaker_on ? 'y' : 'n', main_mic_on ? 'y' : 'n', headset_mic_on ? 'y' : 'n');
 }
 
 /* must be called with hw device and output stream mutexes locked */
